eef40814a33a0bf2d783d5fea857699469fdd01b
toshikihayashi <s1521093@cco.kanagawa-it.ac.jp>
2020-03-03 20:59:24 +0900

    テストケース洗い出し途中

--- Local_Syogi/app/src/test/java/com/example/local_syogi/GameLogicRatePresenterTest.kt
+++ Local_Syogi/app/src/test/java/com/example/local_syogi/GameLogicRatePresenterTest.kt
@@ -21,6 +21,27 @@ class GameLogicRatePresenterTest {
     //テストクラス
     private val presenter:GameLogicRatePresenter = GameLogicRatePresenter(view,repositoryMock)
 
+    //画面タッチ　自分の盤上の駒　盤上のヒント or 盤上のその他 or 持ち駒　or 盤外
+
+    //ヒント取得 全種類の駒で全通りの行ける場所のテスト
+
+    //駒を動かす
+
+    //王手判定
+
+    //将棋盤描画
+
+    //成り判定
+
+    //成る
+
+    //詰み判定
+
+    //持ち駒を使う
+
+
+
+    //ターンチェンジ
     @Test
     fun setTurn(){
         presenter.socketMove(2,6,2,5)
