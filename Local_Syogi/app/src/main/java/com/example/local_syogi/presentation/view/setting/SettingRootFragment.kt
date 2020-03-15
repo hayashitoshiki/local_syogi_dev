a364fd37491113b994c7fbb5db933fe030734c9e
toshikihayashi <tosikihayasi@outlook.jp>
2020-03-15 20:45:11 +0900

    ゲームモード選択画面；View⇔Presenter分離

--- Local_Syogi/app/src/main/java/com/example/local_syogi/presentation/view/setting/SettingRootFragment.kt
+++ Local_Syogi/app/src/main/java/com/example/local_syogi/presentation/view/setting/SettingRootFragment.kt
@@ -15,7 +15,6 @@ import android.widget.TextView
 import android.widget.Toast
 import com.example.local_syogi.R
 import com.example.local_syogi.presentation.contact.SettingRootContact
-import com.example.local_syogi.presentation.contact.UsuallySyogiContact
 import com.example.local_syogi.presentation.view.MainActivity
 import com.example.local_syogi.presentation.view.game.GameRateActivity
 import com.example.local_syogi.syogibase.presentation.view.GameActivity
@@ -29,12 +28,11 @@ class SettingRootFragment  : Fragment(), SettingRootContact.View,OnBackPressedLi
     private val presenter: SettingRootContact.Presenter by inject { parametersOf(this) }
 
 
-    private lateinit var view2:ConstraintLayout
-    private lateinit var tabFrame:FrameLayout
-    private lateinit var modeFrame:FrameLayout
-    private lateinit var title:TextView
+    private lateinit var view2: ConstraintLayout
+    private lateinit var tabFrame: FrameLayout
+    private lateinit var modeFrame: FrameLayout
+    private lateinit var title: TextView
     private var tab = -1
-    private var mode = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,7 +45,8 @@ class SettingRootFragment  : Fragment(), SettingRootContact.View,OnBackPressedLi
         tabFrame = view.findViewById(R.id.tab)
 
         childFragmentManager.beginTransaction()
-            .add(R.id.tab,
+            .add(
+                R.id.tab,
                 SelectNormalFragment.newInstance(
                     FREE,
                     tab
@@ -61,9 +60,12 @@ class SettingRootFragment  : Fragment(), SettingRootContact.View,OnBackPressedLi
 
     override fun onStart() {
         super.onStart()
-        val fade: Animation = AnimationUtils.loadAnimation(context, R.anim.fade_in_slide_new) as Animation
-        val fadeDelay: Animation = AnimationUtils.loadAnimation(context, R.anim.fade_in_slide_new_delay) as Animation
-        val fadeUp: Animation = AnimationUtils.loadAnimation(context, R.anim.fade_in_up_new) as Animation
+        val fade: Animation =
+            AnimationUtils.loadAnimation(context, R.anim.fade_in_slide_new) as Animation
+        val fadeDelay: Animation =
+            AnimationUtils.loadAnimation(context, R.anim.fade_in_slide_new_delay) as Animation
+        val fadeUp: Animation =
+            AnimationUtils.loadAnimation(context, R.anim.fade_in_up_new) as Animation
 
         tabFrame.startAnimation(fade)
         tabFrame.visibility = View.VISIBLE
@@ -77,7 +79,7 @@ class SettingRootFragment  : Fragment(), SettingRootContact.View,OnBackPressedLi
        選択していないボタンは白色にして
        選択しているボタンは指定職にする
        また、fragmentを入れ替える */
-    fun changeMode(fragment: Fragment,tab:Int){
+    fun changeMode(fragment: Fragment, tab: Int) {
         childFragmentManager.beginTransaction()
             .setCustomAnimations(
                 R.anim.fade_in_slide,
@@ -89,69 +91,66 @@ class SettingRootFragment  : Fragment(), SettingRootContact.View,OnBackPressedLi
     }
 
     //タッチイベント
-    fun onTouchEvent(x:Int, y:Int, x2:Int, y2:Int) {
-        if(x <= 400) {
-            if (x2 - x < -10) {
-                if(y in 800..1200){
-                    closeActivity()
-                    val main = activity as MainActivity
-                    main.backFragment()
-                }else {
-                    flipCard(1)
-                }
-            } else if(10 < x2 - x) {
-                flipCard(2)
-            }
-        }
+    fun onTouchEvent(x: Int, y: Int, x2: Int, y2: Int) {
+        presenter.onTouchEvent(x, y, x2, y2)
     }
 
-    //タブ切り替えモーション
-    private fun flipCard(roll:Int) {
-        mode = if(mode == FREE){
-            RATE
-        }else{
-            FREE
-        }
-        if(roll == 1) {
-            childFragmentManager.beginTransaction()
-                .setCustomAnimations(
-                    R.anim.card_flip_right_in,
-                    R.anim.card_flip_right_out
-                )
-                .replace(R.id.tab,
-                    SelectNormalFragment.newInstance(
-                        mode,
-                        tab
-                    )
-                )
-                .commit()
-        }else {
-            childFragmentManager.beginTransaction()
-                .setCustomAnimations(
-                    R.anim.card_flip_left_in,
-                    R.anim.card_flip_left_out
-                )
-                .replace(R.id.tab,
-                    SelectNormalFragment.newInstance(
-                        mode,
-                        tab
-                    )
-                )
-                .commit()
-        }
+    //ホーム画面へ戻る
+    override fun backHome() {
+        closeActivity()
+        val main = activity as MainActivity
+        main.backFragment()
     }
 
-    //対局開始
-    fun fadeOut(){
-        if (mode == FREE) {
-            val intent = Intent(context, GameActivity::class.java)
-            startActivity(intent)
-        }else if(presenter.isAuth()){
-            val intent = Intent(context, GameRateActivity::class.java)
-            startActivity(intent)
-        }else {
-            showAuthToast()
-        }
+    //タブカード回転
+    override fun flipCardRight(mode: Int) {
+        childFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.card_flip_right_in,
+                R.anim.card_flip_right_out
+            )
+            .replace(
+                R.id.tab,
+                SelectNormalFragment.newInstance(
+                    mode,
+                    tab
+                )
+            )
+            .commit()
+    }
+
+    //タブカード回転
+    override fun flipCardLeft(mode: Int) {
+        childFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.card_flip_left_in,
+                R.anim.card_flip_left_out
+            )
+            .replace(
+                R.id.tab,
+                SelectNormalFragment.newInstance(
+                    mode,
+                    tab
+                )
+            )
+            .commit()
+    }
+
+    //対局開始判定ロジックへ
+    fun fadeOut() {
+        presenter.startGame()
+    }
+
+    //通常対戦画面へ遷移
+    override fun startNomarGame() {
+        val intent = Intent(context, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    //通信対戦画面へ遷移
+    override fun startRateGame() {
+        val intent = Intent(context, GameRateActivity::class.java)
+        startActivity(intent)
     }
 
     //activity終了
@@ -180,13 +179,12 @@ class SettingRootFragment  : Fragment(), SettingRootContact.View,OnBackPressedLi
     }
 
     //エラー表示
-    private fun showAuthToast() {
+    override fun showAuthToast() {
         Toast.makeText(context, "ログインしてください", Toast.LENGTH_LONG).show()
     }
 
     companion object {
         const val FREE = 1
-        const val RATE = 2
 
         @JvmStatic
         fun newInstance(): SettingRootFragment {
